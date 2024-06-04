0803082970de67a5050a1259aea0b4548d97ccc2
stauem <77047092+stauem@users.noreply.github.com>
2024-06-04 12:25:21 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import pickle
 import streamlit as st
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
